f3632a68eca25fed5508e46f189c7d400a4304d7
Real_abhi <137689688+Realbhi@users.noreply.github.com>
2026-02-17 13:39:48 +0530

    Add Terraform configuration for EC2 setup
    Define AWS resources including key pair, VPC, security group, and EC2 instance.

---
+++ 01_Terraform_basics/03_Setting-EC2/01.ec2.tf
@@ -0,0 +1,59 @@
+#1. key-pair 
+#2. VPC - Security Group
+#3. Ec2 instance
+
+resource aws_key_pair deployer {
+     key_name = "Deployer-key"
+     public_key = file("prvtpubkey.pub")
+}
+
+resource aws_default_vpc default-vpc{
+     
+}
+
+resource aws_security_group SgforTF {
+   name = "sgfortraffic"
+   vpc_id = aws_default_vpc.default-vpc.id
+   description = "This security group is to control ingress and egress traffic"  
+
+   ingress{
+     from_port = 22
+     to_port = 22
+     cidr_blocks = ["0.0.0.0/0"]
+     protocol = "tcp"
+   }
+   
+   ingress{
+     from_port = 80
+     to_port = 80
+     cidr_blocks = ["0.0.0.0/0"]
+     protocol = "tcp"
+   }
+ 
+   ingress{
+     from_port = 80
+     to_port = 80
+     cidr_blocks = ["0.0.0.0/0"]
+     protocol = "tcp"
+   }
+   
+   egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+}
+}
+
+resource aws_instance "ec2-instance" {
+   ami = var.ami
+   instance_type = var.ec2-instance-type
+   key_name = aws_key_pair.deployer.key_name
+   security_groups = [aws_security_group.SgforTF.name]
+  
+  root_block_device {
+     volume_size = 10
+     volume_type = "gp3"
+  }
+
+}
